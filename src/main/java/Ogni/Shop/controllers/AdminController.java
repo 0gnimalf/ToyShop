1a1ad609c3e86dbb8e3a52f37df9e6ec35b86f32
0gnimalf <ognimalf.xd@gmail.com>
2025-06-09 21:32:33 +0300

    update admin function

--- src/main/java/Ogni/Shop/controllers/AdminController.java
+++ src/main/java/Ogni/Shop/controllers/AdminController.java
@@ -28,6 +28,12 @@ public class AdminController {
     @Autowired
     private ProductRepo productRepo;
 
+    private void returnValuesToModel(Model model){
+        model.addAttribute("groups", groupRepo.findAll());
+        model.addAttribute("materials", materialRepo.findAll());
+        model.addAttribute("types", ProductType.values());
+    }
+
     @GetMapping("/product")
     public String products(Model model,
                            @RequestParam(name = "page", defaultValue = "0") int page,
@@ -51,7 +57,7 @@ public class AdminController {
         if (error!=null && error.equals("duplicate")) {
             model.addAttribute("error", "Такой продукт уже существует");
         }
-        model.addAttribute("groups", groupRepo.findAll());
+        returnValuesToModel(model);
         model.addAttribute("nProduct", new Product());
         return "admin/product/n-product";
     }
@@ -62,6 +68,41 @@ public class AdminController {
         }
         productRepo.save(product);
         return "redirect:/admin/product";
+    }
+
+    @GetMapping("/product/{id}/edit")
+    public String editProduct(@PathVariable Long id, Model model) {
+        Product product = productService.getById(id);
+        model.addAttribute("product", product);
+        returnValuesToModel(model);
+        return "admin/product/edit-product";
+    }
+
+    @PostMapping("/product/{id}")
+    public String updateProduct(@ModelAttribute Product product) {
+        productRepo.save(product);
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/product/{id}/delete")
+    public String deleteProduct(@PathVariable Long id) {
+        productRepo.deleteById(id);
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/material")
+    public String material(Model model,
+                         @RequestParam(name = "page", defaultValue = "0") int page) {
+        int pageSize = 10;
+        Page<Material> list = materialRepo.findAll(PageRequest.of(page, pageSize));
+        if (!list.isEmpty()) {
+            model.addAttribute("list", list);
+            model.addAttribute("currentPage", page);
+            model.addAttribute("totalPages", list.getTotalPages());
+        } else {
+            model.addAttribute("list", Page.empty());
+        }
+        return "admin/material/all-materials";
     }
 
     @GetMapping("/material/new")
@@ -81,13 +122,47 @@ public class AdminController {
         return "redirect:/admin/material";
     }
 
+    @GetMapping("/material/{id}/edit")
+    public String editMaterial(@PathVariable Long id, Model model) {
+        Material material = materialRepo.findById(id).get();
+        model.addAttribute("material", material);
+        returnValuesToModel(model);
+        return "admin/material/edit-material";
+    }
+
+    @PostMapping("/material/{id}")
+    public String updateMaterial(@ModelAttribute Material material) {
+        materialRepo.save(material);
+        return "redirect:/admin/material";
+    }
+
+    @GetMapping("/material/{id}/delete")
+    public String deleteMaterial(@PathVariable Long id) {
+        materialRepo.deleteById(id);
+        return "redirect:/admin/material";
+    }
+
+    @GetMapping("/group")
+    public String groups(Model model,
+                           @RequestParam(name = "page", defaultValue = "0") int page) {
+        int pageSize = 10;
+        Page<ProductGroup> list = groupRepo.findAll(PageRequest.of(page, pageSize));
+        if (!list.isEmpty()) {
+            model.addAttribute("list", list);
+            model.addAttribute("currentPage", page);
+            model.addAttribute("totalPages", list.getTotalPages());
+        } else {
+            model.addAttribute("list", Page.empty());
+        }
+        return "admin/group/all-groups";
+    }
+
     @GetMapping("/group/new")
     public String getNewGroup(Model model, @RequestParam(name = "error", required = false) String error) {
         if (error!=null && error.equals("duplicate")) {
             model.addAttribute("error", "Такая группа уже существует");
         }
-        model.addAttribute("materials", materialRepo.findAll());
-        model.addAttribute("types", ProductType.values());
+        returnValuesToModel(model);
         model.addAttribute("nGroup", new ProductGroup());
         return "/admin/group/n-group";
     }
@@ -97,6 +172,26 @@ public class AdminController {
             return "redirect:/admin/group?error=duplicate";
         }
         groupRepo.save(group);
+        return "redirect:/admin/group";
+    }
+
+    @GetMapping("/group/{id}/edit")
+    public String editGroup(@PathVariable Long id, Model model) {
+        ProductGroup group = groupRepo.findById(id).get();
+        model.addAttribute("group", group);
+        returnValuesToModel(model);
+        return "admin/group/edit-group";
+    }
+
+    @PostMapping("/group/{id}")
+    public String updateGroup(@ModelAttribute ProductGroup group) {
+        groupRepo.save(group);
+        return "redirect:/admin/group";
+    }
+
+    @GetMapping("/group/{id}/delete")
+    public String deleteGroup(@PathVariable Long id) {
+        groupRepo.deleteById(id);
         return "redirect:/admin/group";
     }
 }
